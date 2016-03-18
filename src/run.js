0d0087e60b757116ca1057cb541d52e77daf908b
jedi_knight <zhuyudong@aliyun.com>
2016-03-18 15:42:22 +0800

    add comment

---
+++ src/run.js
@@ -0,0 +1,12 @@
+(function(){
+    var el=document.createElement('script');
+    el.type='text/javascript';
+    //https://micmro.github.io/performance-bookmarklet/dist/performanceBookmarklet.min.js
+    el.src='../dist/performanceBookmarklet.min.js';
+    el.onerror=function(){
+        alert("Looks like the Content Security Policy directive is blocking the use of bookmarklets\n\nYou can copy and paste the content of:\n\n\"https://micmro.github.io/performance-bookmarklet/dist/performanceBookmarklet.min.js\"\n\ninto your console instead\n\n(link is in console already)");
+        console.log("https://micmro.github.io/performance-bookmarklet/dist/performanceBookmarklet.min.js");
+        
+    };
+    document.getElementsByTagName('body')[0].appendChild(el);
+})();
